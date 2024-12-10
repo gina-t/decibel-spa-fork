54e3838125b9c070c9a63c5a330d837781915889
Tao <ginadrcoder@gmail.com>
2024-12-10 20:27:37 +1100

    navbar linking to pages, modified App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import Hero from "./components/Hero";
 import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div>
       <NavBar />
-      <Hero />
       <Outlet />
       <Footer />
     </div>
