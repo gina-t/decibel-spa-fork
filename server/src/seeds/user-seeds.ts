bc978c4050f9b0584ea33b52221489b6a129fa52
Mitch <mitchjoelklein@hotmail.com>
2024-12-16 07:30:02 +1100

    Updated Seeds, fixed auto-increment

--- server/src/seeds/user-seeds.ts
+++ server/src/seeds/user-seeds.ts
@@ -1,9 +1,24 @@
-import { User } from '../models/User.js';
+import { User } from "../models/User.js";
 
 export const seedUsers = async () => {
-  await User.bulkCreate([
-    { id: '1', username: 'JollyGuru', email: 'jollyguru@example.com', password: 'password' },
-    { id: '2', username: 'SunnyScribe', email: 'sunnyscribe@example.com', password: 'password' },
-    { id: '3', username: 'RadiantComet', email: 'radiantcomet@example.com', password: 'password' },
-  ], { individualHooks: true });
+  await User.bulkCreate(
+    [
+      {
+        username: "JollyGuru",
+        email: "jollyguru@example.com",
+        password: "password",
+      },
+      {
+        username: "SunnyScribe",
+        email: "sunnyscribe@example.com",
+        password: "password",
+      },
+      {
+        username: "RadiantComet",
+        email: "radiantcomet@example.com",
+        password: "password",
+      },
+    ],
+    { individualHooks: true }
+  );
 };
